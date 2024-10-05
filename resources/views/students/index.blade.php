5bc1b75bed44113ed28512ec4aea273df6b57520
mahroof-cp <mahroof9726@gmail.com>
2024-10-05 15:21:02 +0530

    test

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -38,7 +38,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
-
 <script type="text/javascript">
     $(document).ready(function() {
         $.ajaxSetup({
